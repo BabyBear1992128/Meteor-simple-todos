feb25c2c97e9d28526d60bfb96aee456544a3417
Baby Pooh <babybearbear1280@gmail.com>
2023-02-08 19:13:59 -0500

    initial commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,100 @@
+import { Meteor } from 'meteor/meteor';
+import React, { useState, Fragment } from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/db/TaskCollection';
+import { Task } from './Task';
+import { TaskForm } from './TaskForm';
+import { LoginForm } from './LoginForm';
+
+const toggleChecked = ({ _id, isChecked }) =>
+    Meteor.call('tasks.setIsChecked', _id, !isChecked);
+
+const deleteTask = ({ _id }) => Meteor.call('tasks.remove', _id);
+
+const logout = () => Meteor.logout();
+
+export const App = () => {
+    const user = useTracker(() => Meteor.user());
+
+    const [hideCompleted, setHideCompleted] = useState(false);
+
+    const hideCompletedFilter = { isChecked: { $ne: true } };
+
+    const userFilter = user ? { userId: user._id } : {};
+
+    const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+
+    const tasks = useTracker(() => {
+        if (!user) {
+            return [];
+        }
+
+        return TasksCollection.find(
+            hideCompleted ? pendingOnlyFilter : userFilter,
+            {
+                sort: { createdAt: -1 },
+            }
+        ).fetch();
+    });
+
+    const pendingTasksCount = useTracker(() => {
+        if (!user) {
+            return 0;
+        }
+
+        return TasksCollection.find(pendingOnlyFilter).count();
+    });
+
+    const pendingTasksTitle = `${
+        pendingTasksCount ? ` (${pendingTasksCount})` : ''
+    }`;
+
+
+    return (
+        <div className="app">
+            <header>
+                <div className="app-bar">
+                    <div className="app-header">
+                        <h1>
+                            📝️ To Do List
+                            {pendingTasksTitle}
+                        </h1>
+                    </div>
+                </div>
+            </header>
+
+            <div className="main">
+                <div className="main">
+                    {user ? (
+                        <Fragment>
+                            <div className="user" onClick={logout}>
+                                {user.username || user.profile.name} 🚪
+                            </div>
+
+                            <TaskForm user={user} />
+
+                            <div className="filter">
+                                <button onClick={() => setHideCompleted(!hideCompleted)}>
+                                    {hideCompleted ? 'Show All' : 'Hide Completed'}
+                                </button>
+                            </div>
+
+                            <ul className="tasks">
+                                {tasks.map(task => (
+                                    <Task
+                                        key={task._id}
+                                        task={task}
+                                        onCheckboxClick={toggleChecked}
+                                        onDeleteClick={deleteTask}
+                                    />
+                                ))}
+                            </ul>
+                        </Fragment>
+                    ) : (
+                        <LoginForm />
+                    )}
+                </div>
+            </div>
+        </div>
+    );
+};
---
+++ server/main.js
@@ -0,0 +1,49 @@
+import { Meteor } from 'meteor/meteor';
+import { Accounts } from 'meteor/accounts-base';
+import { TasksCollection } from '/imports/db/TaskCollection';
+import { ServiceConfiguration } from 'meteor/service-configuration';
+import '/imports/api/tasksMethods';
+
+const SEED_USERNAME = 'meteorite';
+const SEED_PASSWORD = 'password';
+
+const insertTask = (taskText, user) =>
+    TasksCollection.insert({
+        text: taskText,
+        userId: user._id,
+        createdAt: new Date(),
+    });
+
+Meteor.startup(() => {
+    if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+        Accounts.createUser({
+            username: SEED_USERNAME,
+            password: SEED_PASSWORD,
+        });
+    }
+
+    const user = Accounts.findUserByUsername(SEED_USERNAME);
+
+    if (TasksCollection.find().count() === 0) {
+        [
+            'First Task',
+            'Second Task',
+            'Third Task',
+            'Fourth Task',
+            'Fifth Task',
+            'Sixth Task',
+            'Seventh Task'
+        ].forEach(taskText => insertTask(taskText, user));
+    }
+});
+
+ServiceConfiguration.configurations.upsert(
+    { service: 'github' },
+    {
+        $set: {
+            loginStyle: 'popup',
+            clientId: 'd244670c85c726760a87', // insert your clientId here
+            secret: '36d9b74eca2656aad6ef188bc2670d433a9376de', // insert your secret here
+        },
+    }
+);
